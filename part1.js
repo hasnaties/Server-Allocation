2abacfa4ef7415f124271244a6837a362431c061
hasnat-hybrid <hasnaties@gmail.com>
2022-08-05 01:16:59 +0500

    Tested and Done.

---
+++ part1.js
@@ -0,0 +1,45 @@
+const nextServerArray = (array) => {
+
+    //-- Validate Array --
+    if (array.length == -1) {
+        return 'Array is empty!';
+    }
+
+    // Find Smallest and greatest in the array.
+    var smallest = 1;
+    var greatest = array[0];
+
+    for (let index = 1; index < array.length; index++) {
+        
+        if (array[index] < smallest) {
+            smallest = array[index]
+        }
+        if (array[index] > greatest) {
+            greatest = array[index]
+        }
+    }
+    
+    // -- Exception for Number 1 --
+    if (!array.includes(smallest)) {
+        return smallest;
+    }
+
+    // -- Find Available --
+    let temp = smallest;
+    var result = undefined;
+
+    while (temp <= greatest) {
+        if (!array.includes(temp + 1)) {
+            result = temp + 1;
+            break;
+        } else {
+            temp++;
+        }
+    }
+
+    // -- returning Next Server --
+    delete temp;
+    return result;
+}
+
+module.exports = nextServerArray;
---
+++ part2.js
@@ -0,0 +1,61 @@
+const nextServerArray = require('./part1')
+
+/* 
+    A Global array to keep track for both
+    server allocation and deallocation functions.
+*/
+const serverArray = []
+
+/* 
+    -- Allocates Server for regions.
+    -- returns String of available server
+    for specific region.
+*/
+const allocate = (region) => {
+
+    // -- For First server within region.
+    if (!serverArray.some((obj) => obj.region == region)) {
+        serverArray.push({
+            region,
+            server: 1
+        })
+        return region+1;
+    }
+
+    // Get alloted server for specific region.
+    const tempArray = serverArray.filter((obj) => obj.region == region);
+    const allocatedServersArray = [];
+
+    tempArray.forEach((obj) => {
+        allocatedServersArray.push(obj.server)
+    })
+
+    //-- Gets NextServer
+    const availableServer = nextServerArray(allocatedServersArray);
+
+    // Allocating and returning the available server.
+    serverArray.push({ region, server: availableServer});
+    delete tempArray;
+    return region+availableServer;
+}
+
+/* 
+    -- Deallocates Server for regions.
+    -- returns number of new array size after
+    removing the server.
+*/
+const deallocate = (server) => {
+
+    // -- Getting server No.
+   const freeServer = server[server.length - 1];
+   
+   // Searching for the server Index.
+   const serverIndex = serverArray.findIndex((element) => {
+        return element.server == freeServer
+    })
+    
+    // -- Removing Server from allocated array.
+    if (serverIndex !== -1) {
+        serverArray.splice(serverIndex, 1)[0]
+    }
+}
